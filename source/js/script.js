70792d7210c99819177d45173e678e9833d88697
Md Sojon Islam <sojonatmail@gmail.com>
2020-12-29 16:54:41 +0600

    initialize template

---
+++ source/js/script.js
@@ -0,0 +1,63 @@
+$(window).on('load', function () {
+  'use strict';
+
+  // preloader
+  $('#preloader').fadeOut('slow');
+  $('body').css({
+    'overflow': 'visible'
+  });
+
+
+  //  isotope
+  var $container = $('.portfolio_container');
+  $container.isotope({
+    filter: '*'
+  });
+
+  $('.portfolio_filter a').click(function () {
+    $('.portfolio_filter .active').removeClass('active');
+    $(this).addClass('active');
+
+    var selector = $(this).attr('data-filter');
+    $container.isotope({
+      filter: selector,
+      animationOptions: {
+        duration: 500,
+        animationEngine: 'jquery'
+      }
+    });
+    return false;
+  });
+
+  // back to top
+  var offset = 300,
+  offset_opacity = 500,
+  scroll_top_duration = 700,
+  $back_to_top = $('.cd-top');
+
+  //hide or show the "back to top" link
+  $(window).scroll(function () {
+    ($(this).scrollTop() > offset) ? $back_to_top.addClass('cd-is-visible'): $back_to_top.removeClass('cd-is-visible cd-fade-out');
+    if ($(this).scrollTop() > offset_opacity) {
+      $back_to_top.addClass('cd-fade-out');
+    }
+  });
+
+  //smooth scroll to top
+  $back_to_top.on('click', function (event) {
+    event.preventDefault();
+    $('body,html').animate({
+      scrollTop: 0
+    }, scroll_top_duration);
+  });
+
+  // input
+  $('.input-contact input, .textarea-contact textarea').focus(function () {
+    $(this).next('span').addClass('active');
+  });
+  $('.input-contact input, .textarea-contact textarea').blur(function () {
+    if ($(this).val() === '') {
+      $(this).next('span').removeClass('active');
+    }
+  });
+});
